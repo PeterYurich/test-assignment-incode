b00f8df3b390446e37c91fd781566a7efe8f88cb
Peter <peteryurich@gmail.com>
2023-04-19 18:27:10 +0300

    add dnd to other board with cards

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,69 +1,42 @@
 import React, { useEffect, useState } from 'react'
 import css from "./dashboard.module.css"
-import { Paper, Typography, Box, Divider } from '@mui/material'
+import { Paper, Typography, Box, Divider, List, ListItem } from '@mui/material'
 import { useSelector } from 'react-redux'
 import { selectRepo } from 'redux/repo/repoSelectors'
 import storage from 'utils/storage'
 
 export default function Dashboard() {
+    const [heldIssueId, setHeldIssueId] = useState()
+    const [startBoardIndex, setStartBoardIndex] = useState()
+
     const currentRepo = useSelector(selectRepo)
-    const [allRepoBoards] = useState(storage.load('allRepoBoards') || [])
-    const [boardsState, setBoardsState] = useState([
+    const savedBoardState = storage.load(`${currentRepo.id}`)
+    const [boardsState, setBoardsState] = useState(savedBoardState || [
         { id: 1, title: 'To Do', items: [] },
-        { id: 2, title: 'In Progress', items: [1667358069] },
-        { id: 3, title: 'Done', items: [1673582916] }
+        { id: 2, title: 'In Progress', items: [] },
+        { id: 3, title: 'Done', items: [] }
     ])
 
-    const [toDoCards, setToDoCards] = useState([])
-    const [inProgressCards, setInProgressCards] = useState([])
-    const [doneCards, setDoneCards] = useState([])
-
-    const [heldCard, setHeldCard] = useState()
-
     useEffect(() => {
-        const toDoItems = currentRepo.issues.map((issue, index) => issue.id)
-
-        const newBoardsState = [...boardsState]
-        // const toDoIndex = newBoardsState.indexOf(board => board.title === "To Do")
-        newBoardsState[0].items = toDoItems
-
-        setBoardsState(newBoardsState)
-
-        const currentRepoBoards = {
-            repoId: currentRepo.id,
-            boardsState
+        if (!savedBoardState) {
+            const allIssuesIds = currentRepo?.issues.map(issue => issue.id)
+            const newBoardsState = [...boardsState]
+            newBoardsState[0].items = allIssuesIds
+            setBoardsState(newBoardsState)
+            return
         }
-        console.log('currentRepoBoards: ', currentRepoBoards);
-        storage.save('allRepoBoards', [currentRepoBoards, ...allRepoBoards])
+        setBoardsState(savedBoardState)
 
-    }, [currentRepo, allRepoBoards])
-
-    useEffect(() => {
-        if (currentRepo && boardsState[0].items.length > 0) {
-            // const toDoContent = new Set(boardsState[0].items.map(issue => issue.id));
-            const toDoCards = currentRepo.issues.filter(issue => boardsState[0].items.includes(issue.id));
-            setToDoCards(toDoCards)
-        }
-        if (currentRepo && boardsState[1].items.length > 0) {
-            // const inProgressContent = new Set(boardsState[1].items.map(issue => issue.id));
-            const inProgressCards = currentRepo.issues.filter(issue => boardsState[1].items.includes(issue.id));
-            setInProgressCards(inProgressCards)
-        }
-        if (currentRepo && boardsState[2].items.length > 0) {
-            // const doneContent = new Set(boardsState[2].items.map(issue => issue.id));
-            const doneCards = currentRepo.issues.filter(issue => boardsState[2].items.includes(issue.id));
-            setDoneCards(doneCards)
-        }
+    }, [currentRepo])
 
 
-    }, [currentRepo, boardsState])
-
-
-    const dragStartHandler = (e, issue) => {
-        setHeldCard(issue)
+    const dragStartHandler = (e, issueId, boardIndex) => {
+        setHeldIssueId(issueId)
+        setStartBoardIndex(boardIndex)
     }
+
     const dragLeaveHandler = (e) => {
-        e.target.style.background = 'white'
+        // e.target.style.background = 'white'
 
     }
 
@@ -71,149 +44,77 @@ export default function Dashboard() {
         e.preventDefault()
     }
 
-    const dragOverHandler = (e, issue) => {
+    const dragOverHandler = (e) => {
         e.preventDefault()
-        e.target.style.background = 'lightgray'
+        if (e.target.tagName == 'DIV') {
+            e.target.style.backgroundColor = 'lightgray'
+            // e.target.style.boxShadow = '0 2px 3px gray'
+        }
     }
 
-    const dropHandler = (e, issue) => {
+    const dropHandler = (e, finishIssueId, finishBoardIndex) => {
         e.preventDefault()
+        const newBoardsState = [...boardsState]
+        const heldIssueIndex = boardsState[startBoardIndex].items.indexOf(heldIssueId)
+        newBoardsState[startBoardIndex].items.splice(heldIssueIndex, 1)
+        newBoardsState[finishBoardIndex].items.splice(finishIssueId, 0, heldIssueId)
+        setBoardsState(newBoardsState)
+    }
+
+    const dropCardHandler = () => {
+
     }
 
     return (
         <Box>
-            <Paper className={css.mainBoard} elevation={3} >
-                {boardsState.map(board => (
-                    <Paper className={css.taskBoard} key={board.id} >
-                        <Typography variant='h5'
-                            className={css.boardTitle}>{board.title}
+            <List className={css.boardWrapper}>
+                {boardsState.map((board, boardIndex) => (
+                    <ListItem className={css.board} key={board.id}
+                    onDragOver={(e) => dragOverHandler(e)}
+                    onDrop={(e) => dropCardHandler(e, board)}
+                    >
+                        <Typography variant='h5' className={css.boardTitle}>
+                            {board.title}
                         </Typography>
-                        {
-                            currentRepo.issues.map(issue => (
-                                board.items.includes(issue.id) &&
-                                <Paper className={css.issueCard} key={issue.id}
-                                    draggable={true}
-                                    onDragStart={(e) => dragStartHandler(e, issue)}
-                                    onDragLeave={(e) => dragLeaveHandler(e)}
-                                    onDragEnd={(e) => dragEndHandler(e)}
-                                    onDragOver={(e) => dragOverHandler(e)}
-                                    onDrop={(e) => dropHandler(e, issue)}
-                                >
-                                    <Typography className={css.issueTitle}>
-                                        {issue.title}
-                                    </Typography>
-                                    <Box className={css.rowFlexBox}>
-                                        <Typography>#{issue.number}</Typography>
-                                        <Typography>{issue.openedAt}</Typography>
-                                    </Box>
-                                    <Box className={css.rowFlexBox}>
-                                        <Typography>{issue.author}</Typography>
-                                        <Divider flexItem orientation="vertical" />
-                                        <Typography> Comments: {issue.comments} </Typography>
-                                    </Box>
-                                </Paper>
-                            ))
-                        }
-                    </Paper>
+                        <Paper  className={css.boardPaper}>
+                            <List>
+                                {board.items.length > 0 && board.items.map((issueId, index, arr) => {
+                                    const cardContent = currentRepo.issues.find(issue => issue.id === issueId)
+                                    if (!cardContent) {
+                                        arr.splice(index, 1)
+                                        return
+                                    }
+                                    return (
+                                        <ListItem name='card' key={issueId} sx={{ p: 0 }}
+                                            draggable={true}
+                                            onDragStart={(e) => dragStartHandler(e, issueId, boardIndex)}
+                                            onDragLeave={(e) => dragLeaveHandler(e)}
+                                            onDragEnd={(e) => dragEndHandler(e)}
+                                            onDragOver={(e) => dragOverHandler(e)}
+                                            onDrop={(e) => dropHandler(e, issueId, boardIndex)}
+                                        >
+                                            <Paper className={css.issueCard} >
+                                                <Typography className={css.issueTitle}>
+                                                    {cardContent?.title}
+                                                </Typography>
+                                                <Box className={css.rowFlexBox}>
+                                                    <Typography>#{cardContent.number}</Typography>
+                                                    <Typography>{cardContent.openedAt}</Typography>
+                                                </Box>
+                                                <Box className={css.rowFlexBox}>
+                                                    <Typography>{cardContent.author}</Typography>
+                                                    <Divider flexItem orientation="vertical" />
+                                                    <Typography> Comments: {cardContent.comments} </Typography>
+                                                </Box>
+                                            </Paper>
+                                        </ListItem>
+                                    )
+                                })}
+                            </List>
+                        </Paper>
+                    </ListItem>
                 ))}
-                {/* <div>
-                    <Paper className={css.taskBoard}>
-                        <Typography variant='h5'
-                            className={css.boardTitle}>To Do
-                        </Typography>
-                        {toDoCards && toDoCards.length > 0 &&
-                            toDoCards.map(issue => (
-                                <div className={css.issueCardWrapper} key={issue.id}
-                                    draggable={true}
-                                    onDragStart={(e) => dragStartHandler(e, issue)}
-                                    onDragLeave={(e) => dragLeaveHandler(e)}
-                                    onDragEnd={(e) => dragEndHandler(e)}
-                                    onDragOver={(e) => dragOverHandler(e)}
-                                    onDrop={(e) => dropHandler(e, issue)}
-                                >
-                                    <Paper className={css.issueCard}>
-                                        <Typography className={css.issueTitle}>
-                                            {issue.title}
-                                        </Typography>
-                                        <Box className={css.rowFlexBox}>
-                                            <Typography>#{issue.number}</Typography>
-                                            <Typography>{issue.openedAt}</Typography>
-                                        </Box>
-                                        <Box className={css.rowFlexBox}>
-                                            <Typography>{issue.author}</Typography>
-                                            <Divider flexItem orientation="vertical" />
-                                            <Typography> Comments: {issue.comments} </Typography>
-                                        </Box>
-                                    </Paper>
-                                </div>
-                            ))}
-                    </Paper>
-                    <Paper className={css.taskBoard}>
-                        <Typography variant='h5'
-                            className={css.boardTitle}>In Progress
-                        </Typography>
-                        {inProgressCards && inProgressCards.length > 0 &&
-                            inProgressCards.map(issue => (
-                                <div className={css.issueCardWrapper} key={issue.id}
-                                    draggable={true}
-                                    onDragStart={(e) => dragStartHandler(e, issue)}
-                                    onDragLeave={(e) => dragLeaveHandler(e)}
-                                    onDragEnd={(e) => dragEndHandler(e)}
-                                    onDragOver={(e) => dragOverHandler(e)}
-                                    onDrop={(e) => dropHandler(e, issue)}
-                                >
-                                    <Paper className={css.issueCard}>
-                                        <Typography className={css.issueTitle}>
-                                            {issue.title}
-                                        </Typography>
-                                        <Box className={css.rowFlexBox}>
-                                            <Typography>#{issue.number}</Typography>
-                                            <Typography>{issue.openedAt}</Typography>
-                                        </Box>
-                                        <Box className={css.rowFlexBox}>
-                                            <Typography>{issue.author}</Typography>
-                                            <Divider flexItem orientation="vertical" />
-                                            <Typography> Comments: {issue.comments} </Typography>
-                                        </Box>
-                                    </Paper>
-                                </div>
-                            ))}
-
-                    </Paper>
-                    <Paper className={css.taskBoard}>
-                        <Typography variant='h5'
-                            className={css.boardTitle}>Done
-                        </Typography>
-                        {doneCards && doneCards.length > 0 &&
-                            doneCards.map(issue => (
-                                <div className={css.issueCardWrapper} key={issue.id}
-                                    draggable={true}
-                                    onDragStart={(e) => dragStartHandler(e, issue)}
-                                    onDragLeave={(e) => dragLeaveHandler(e)}
-                                    onDragEnd={(e) => dragEndHandler(e)}
-                                    onDragOver={(e) => dragOverHandler(e)}
-                                    onDrop={(e) => dropHandler(e, issue)}
-                                >
-                                    <Paper className={css.issueCard}>
-                                        <Typography className={css.issueTitle}>
-                                            {issue.title}
-                                        </Typography>
-                                        <Box className={css.rowFlexBox}>
-                                            <Typography>#{issue.number}</Typography>
-                                            <Typography>{issue.openedAt}</Typography>
-                                        </Box>
-                                        <Box className={css.rowFlexBox}>
-                                            <Typography>{issue.author}</Typography>
-                                            <Divider flexItem orientation="vertical" />
-                                            <Typography> Comments: {issue.comments} </Typography>
-                                        </Box>
-                                    </Paper>
-                                </div>
-                            ))}
-
-                    </Paper>
-                </div> */}
-            </Paper>
+            </List>
         </Box>
     )
 }
